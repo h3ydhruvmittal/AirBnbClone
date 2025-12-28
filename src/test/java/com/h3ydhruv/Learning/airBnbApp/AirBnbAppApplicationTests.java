4fa509c870625c5170432e4581b2155faac452dc
Dhruv <h3ydhruv@Dhruvs-MacBook-Air.local>
2025-12-29 01:26:05 +0530

    setup

---
+++ src/test/java/com/h3ydhruv/Learning/airBnbApp/AirBnbAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.h3ydhruv.Learning.airBnbApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirBnbAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
